b26bbb836beb1267cb437d825bb707187fd1711d
xiangxi90 <2651171386@qq.com>
2022-05-10 01:14:01 +0800

    complete some options

--- rstat/src/kstat.rs
+++ rstat/src/kstat.rs
@@ -1,11 +1,14 @@
+use std::fmt::Display;
+
+use libc::lstat;
 pub use libc::{stat,statfs};
-//use chrono::prelude::*;
+use chrono::prelude::*;
 
 #[allow(dead_code)]
 pub struct FileStat{
     filename: String,               //文件名
-    stat: stat,                     //
-    statfs:statfs,                  //
+    stat: stat,                     //文件信息
+    statfs:statfs,                  //文件系统信息
 
     blinker : bool,                 //该文件是否是一个链接
     oriname : String,               //链接指向的源文件的名字
@@ -108,7 +111,7 @@ impl OptionSelected{
             else if iter.starts_with('-'){
                 for ch in iter.chars(){
                     match ch{
-                        'l' => self.blink=true,
+                        'L' => self.blink=true,
                         'c' => {
                             self.bfilter = true;
                             self.needflag=NeedFlag::Need;
@@ -141,8 +144,8 @@ impl OptionSelected{
         }
 
         // 获取本地时间偏移量,即所处时区
-        // let dt = Local::now();
-        // println!("dt: {}", dt.offset());
+        let dt = Local::now().offset().to_string();
+        //println!("dt: {}", dt.offset());
 
         
         // 遍历所有文件,并按照选项拿出我们需要的信息
@@ -171,7 +174,12 @@ impl OptionSelected{
             OptionSelected::output_with_fileter(self.output_inf.clone(),filestats);
         }
         else {
-            OptionSelected::output_normal(filestats);
+            if self.bfilesystem{
+                OptionSelected::output_normal_filesystem(filestats,&dt);
+            }
+            else{
+                OptionSelected::output_normal_file(filestats,&dt);
+            }
         }
         Ok(())
     }
@@ -187,11 +195,11 @@ impl OptionSelected{
         for iter in filestat{
             if iter.alive{
                 let filestat =&iter.stat;
-                println!("{} {} {} {:x} {} {} {}{} {} {} {} {} {} {} {} {} {}",
+                println!("{} {} {} {:x} {} {} {:x} {} {} {} {} {} {} {} {} {}",
                     iter.filename,
                     filestat.st_size,filestat.st_blocks,
                     filestat.st_mode,filestat.st_uid,filestat.st_gid,
-                    get_dev_major(filestat.st_dev),get_dev_minor(filestat.st_dev),
+                    filestat.st_dev,
                     filestat.st_ino,filestat.st_nlink,0,0,
                     filestat.st_atime,filestat.st_mtime,filestat.st_ctime,
                     0,filestat.st_blksize,
@@ -221,10 +229,48 @@ impl OptionSelected{
         }
     }
 
-    pub fn output_normal(filestat: Vec<FileStat>){
-        println!("{}:","normal");
+    pub fn output_normal_file(filestat: Vec<FileStat> , offset : &str){
+        println!("{}:","normal  file");
         for iter in filestat{
-            print!("{}   ",iter.filename);
+            if !iter.alive{
+                println!("stat: cannot stat '{}': {}",iter.filename,iter.err_info);
+                continue;
+            }
+            let filestat =&iter.stat;
+            let ftype = FileType::get_file_type(filestat.st_mode);
+            let fpermission = FilePermission::new(filestat.st_mode);
+
+            if iter.blinker{
+                println!("  File: {} -> {}",iter.filename,iter.oriname);
+            }
+            else{
+                println!("  File: {}",iter.filename);
+            }
+            println!("  Size: {:<16}Blocks: {:<11}IO Block: {:<7}{}"
+                ,filestat.st_size,filestat.st_blocks,filestat.st_blksize,
+                ftype);
+            println!("Device: {:<16}Inode: {:<12}Links: {}",
+                format!("{:x}h/{}d",filestat.st_dev,filestat.st_dev),
+                filestat.st_ino,filestat.st_nlink);
+            println!("Access: ({}/{}{})  Uid: ({:>5}/{:>8})   Gid: ({:>5}/{:>8})",
+                fpermission.output_num(),ftype.output_short(),fpermission.output_char(),
+                filestat.st_uid,get_username_with_id(filestat.st_uid),
+                filestat.st_gid,get_groupname_with_id(filestat.st_gid)
+            );
+
+            println!("Access: {} {}\nModify: {} {}\nChange: {} {}\nBirth: -",
+                    get_time_utc2local(filestat.st_atime, filestat.st_atime_nsec),offset,
+                    get_time_utc2local(filestat.st_mtime, filestat.st_mtime_nsec),offset,
+                    get_time_utc2local(filestat.st_ctime, filestat.st_ctime_nsec),offset,
+                );
+
+        }print!("\n");
+    }
+
+    pub fn output_normal_filesystem(filestat: Vec<FileStat> , offset : &str){
+        println!("{}:{}","normal  filesys",offset);
+        for _iter in filestat{
+            
         }print!("\n");
     }
 }
@@ -232,7 +278,7 @@ impl OptionSelected{
 fn get_file_stat(filename : String) -> FileStat{
     unsafe{
         let mut kstat = get_new_stat();
-        let ret = stat(filename.as_ptr().cast(),(&mut kstat) as *mut stat);
+        let ret = lstat(filename.as_ptr().cast(),(&mut kstat) as *mut stat);
         if ret==-1{
             FileStat{
                 filename: filename,
@@ -245,12 +291,16 @@ fn get_file_stat(filename : String) -> FileStat{
             }
         }
         else{
+            let blnk=is_symbolic_link(kstat.st_mode);
             FileStat{
-                filename: filename,
+                filename: filename.clone(),
                 stat: kstat,
                 statfs: get_new_statfs(),
-                blinker: false, 
-                oriname: String::new(), 
+                blinker: blnk, 
+                oriname: match blnk{
+                    true => get_link_oriname(filename),
+                    false => String::new(),
+                } ,
                 alive: true,
                 err_info: String::new(),
             }
@@ -267,7 +317,7 @@ fn get_file_stat_link(filename: String) ->FileStat{
                 filename: filename,
                 stat: get_new_stat(),
                 statfs: get_new_statfs(),
-                blinker: true, 
+                blinker: false, 
                 oriname: String::new(), 
                 alive: false,
                 err_info: std::io::Error::last_os_error().to_string(),
@@ -278,8 +328,8 @@ fn get_file_stat_link(filename: String) ->FileStat{
                 filename: filename.clone(),
                 stat: klstat,
                 statfs: get_new_statfs(),
-                blinker: true, 
-                oriname: get_link_oriname(filename),
+                blinker: false, 
+                oriname: String::new(),
                 alive: true,
                 err_info: String::new(),
             }
@@ -319,15 +369,14 @@ fn get_file_statfs(filename : String) ->FileStat{
 
 fn get_link_oriname(linkname: String) -> String{
     unsafe{
-        let mut namebuf : [char; 50] = [' ';50];
-        let ret=libc::readlink(linkname.as_ptr().cast(),namebuf.as_mut_ptr().cast(),50);
+        let mut namebuf : [i8; 50] = [0;50];
+        let charbuf = namebuf.as_mut_ptr().cast();
+        let ret=libc::readlink(linkname.as_ptr().cast(),charbuf,50);
         if ret!=-1 && ret<50{
-            let mut lname=String::new();
-            for i in namebuf.iter(){
-                lname.push(*i);
-            }
-            return lname;
+            let res=std::ffi::CStr::from_ptr(charbuf);
+            return format!("{}",res.to_str().unwrap());
         }
+        println!("err");
         return linkname;
     }
 }
@@ -351,14 +400,257 @@ pub fn get_new_statfs() -> statfs{
     }
 }
 
+#[allow(dead_code)]
 pub fn get_dev_major(devno: u64) -> u32{
     unsafe{
         return libc::major(devno);
     }
 }
 
+#[allow(dead_code)]
 pub fn get_dev_minor(devno: u64) -> u32{
     unsafe{
         return libc::minor(devno);
     }
+}
+
+/// Encoding of the file mode.  
+/// #define	__S_IFMT	0170000	/* These bits determine file type.  
+/// File types. 
+/// #define	__S_IFDIR	0040000	 Directory. 
+// #define	__S_IFCHR	0020000	 Character device.  
+// #define	__S_IFBLK	0060000	 Block device. 
+// #define	__S_IFREG	0100000	 Regular file.  
+// #define	__S_IFIFO	0010000	 FIFO.  
+// #define	__S_IFLNK	0120000	 Symbolic link.  
+// #define	__S_IFSOCK	0140000	 Socket.  
+
+/// Protection bits.  
+
+/// #define	__S_ISUID	04000	 Set user ID on execution.  
+/// #define	__S_ISGID	02000	 Set group ID on execution.  
+/// #define	__S_ISVTX	01000	 Save swapped text after use (sticky).  
+///#define	__S_IREAD	0400	 Read by owner. 
+/// #define	__S_IWRITE	0200	 Write by owner.  
+/// #define	__S_IEXEC	0100	 Execute by owner.  
+/// 
+
+type mode_t = u32;
+pub const S_IFIFO: mode_t = 4096;
+pub const S_IFCHR: mode_t = 8192;
+pub const S_IFBLK: mode_t = 24576;
+pub const S_IFDIR: mode_t = 16384;
+pub const S_IFREG: mode_t = 32768;
+pub const S_IFLNK: mode_t = 40960;
+pub const S_IFSOCK: mode_t = 49152;
+pub const S_IFMT: mode_t = 61440;
+
+//pub const  FILEPERMIS : mode_t = 4095;
+// pub const S_IRWXU: mode_t = 448;
+// pub const S_IXUSR: mode_t = 64;
+// pub const S_IWUSR: mode_t = 128;
+// pub const S_IRUSR: mode_t = 256;
+// pub const S_IRWXG: mode_t = 56;
+// pub const S_IXGRP: mode_t = 8;
+// pub const S_IWGRP: mode_t = 16;
+// pub const S_IRGRP: mode_t = 32;
+// pub const S_IRWXO: mode_t = 7;
+// pub const S_IXOTH: mode_t = 1;
+// pub const S_IWOTH: mode_t = 2;
+// pub const S_IROTH: mode_t = 4;
+
+#[inline]
+pub fn is_symbolic_link(mode: u32) ->bool{
+    if mode & (S_IFMT) == S_IFLNK{
+        return true;
+    }
+    return false;
+}
+
+
+
+enum FileType{
+    Block,
+    Character,
+    Directory,
+    Link,
+    Regular,
+    Socket,
+    Pipe,
+    Unknown,
+}
+
+impl Display for FileType{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self{
+            FileType::Block => write!(f,"block device"),
+            FileType::Character => write!(f,"character device"),
+            FileType::Directory => write!(f,"directory"),
+            FileType::Link => write!(f,"symbolic link"),
+            FileType::Regular => write!(f,"regular file"),
+            FileType::Socket => write!(f,"socket"),
+            FileType::Pipe => write!(f,"pipe"),
+            FileType::Unknown => write!(f,"unknow type"), 
+        }
+    }
+}
+
+impl FileType{
+    pub fn output_short(&self) -> char{
+        match self{
+            FileType::Block => 'b',
+            FileType::Character => 'c',
+            FileType::Directory => 'd',
+            FileType::Link => 'l',
+            FileType::Regular => '-',
+            FileType::Socket => 's',
+            FileType::Pipe => 'p',
+            FileType::Unknown => 'r', 
+        }
+    }
+}
+
+impl  FileType {
+    pub fn get_file_type(mode: u32) -> Self{
+        let cmp = mode & (S_IFMT);
+        //println!("{}  {} ",mode,cmp);
+        if cmp==S_IFBLK{
+            FileType::Block
+        }else if cmp==S_IFCHR{
+            FileType::Character
+        }else if cmp==S_IFDIR{
+            FileType::Directory
+        }else if cmp==S_IFIFO{
+            FileType::  Pipe
+        }else if cmp==S_IFLNK{
+            FileType::Link
+        }else if cmp==S_IFREG{
+            FileType::Regular
+        }else if cmp==S_IFSOCK{
+            FileType::Socket
+        }else{
+            FileType::Unknown
+        }
+    } 
+}
+struct FilePermission{
+    //filetype: FileType, 
+    owner : &'static str,    //rwx
+    group : &'static str,
+    other : &'static str,
+    //special : &'static str,
+    nowner : i8,
+    ngroup : i8,
+    nother : i8,
+    nspe : i8,
+}
+
+
+impl FilePermission{
+    pub fn new(mode: u32) -> Self{
+        let group_per: &str;
+        let ngroup : i8;
+        match mode&448{
+            448 => {group_per="rwx";ngroup=7},
+            384 => {group_per="rw-";ngroup=6},
+            320 => {group_per="r-x";ngroup=5},    
+            256 => {group_per="r--";ngroup=4},
+            192 => {group_per="-wx";ngroup=3},
+            128 => {group_per="-w-";ngroup=2},
+            64  => {group_per="--x";ngroup=1},
+            0   => {group_per="---";ngroup=0},
+            _   => {panic!("wrong input mode in group")}
+        };
+        let owner_per: &str;
+        let nowner : i8;
+        match mode&56{
+            56 => {owner_per="rwx";nowner=7},
+            48 => {owner_per="rw-";nowner=6},
+            40 => {owner_per="r-x";nowner=5},    
+            32 => {owner_per="r--";nowner=4},
+            24 => {owner_per="-wx";nowner=3},
+            16 => {owner_per="-w-";nowner=2},
+            8  => {owner_per="--x";nowner=1},
+            0  => {owner_per="---";nowner=0},
+            _   => {panic!("wrong input mode in owner")}
+        };
+        let other_per:&str;
+        let nother : i8;
+        match mode&7{
+            7 => {other_per="rwx";nother=7},
+            6 => {other_per="rw-";nother=6},
+            5 => {other_per="r-x";nother=5},    
+            4 => {other_per="r--";nother=4},
+            3 => {other_per="-wx";nother=3},
+            2 => {other_per="-w-";nother=2},
+            1  => {other_per="--x";nother=1},
+            0  => {other_per="---";nother=0},
+            _   => {panic!("wrong input mode in other")}
+        };
+        //let spe_per: &str;
+        let nspe : i8;
+        match mode&3584{
+            3584 => {nspe=7},
+            3072 => {nspe=6},
+            2560 => {nspe=5},    
+            2048 => {nspe=4},
+            1536 => {nspe=3},
+            1024 => {nspe=2},
+            512  => {nspe=1},
+            0  => {nspe=0},
+            _   => {panic!("wrong input mode in special")}
+        };
+        Self{
+            owner: owner_per,
+            group: group_per,
+            other: other_per,
+        //    special: spe_per,
+            ngroup : ngroup,
+            nother : nother,
+            nspe : nspe,
+            nowner : nowner,
+        }
+    }
+
+    pub fn output_num(&self) -> String {
+        format!("{}{}{}{}",self.nspe,self.ngroup,self.nowner,self.nother)
+    }
+
+    pub fn output_char(&self) -> String{
+        format!("{}{}{}",self.group,self.owner,self.other)
+    }
+}
+
+pub fn get_username_with_id(uid: u32) -> String{
+    unsafe{
+        //let userinfo=std::mem::zeroed::<libc::passwd>();
+        let userinfo = libc::getpwuid(uid);
+        if userinfo.is_null(){
+            format!("nullptr")
+        }
+        else{
+            let sde = std::ffi::CStr::from_ptr((*userinfo).pw_name);
+            format!("{}",sde.to_str().unwrap())
+        }
+    }
+}
+
+pub fn get_groupname_with_id(gid: u32) -> String{
+    unsafe{
+        //let userinfo=std::mem::zeroed::<libc::passwd>();
+        let groupinfo = libc::getgrgid(gid);
+        if groupinfo.is_null(){
+            format!("nullptr")
+        }
+        else{
+            let sde = std::ffi::CStr::from_ptr((*groupinfo).gr_name);
+            format!("{}",sde.to_str().unwrap())
+        }
+    }
+}
+
+pub fn get_time_utc2local(tc: i64, tn: i64) -> String{
+    let da : DateTime<Utc> = DateTime::from_utc(NaiveDateTime::from_timestamp(tc, tn.try_into().unwrap()), Utc);
+    let lc : DateTime<Local> = DateTime::from(da);
+    lc.format("%Y-%m-%d %H:%M:%S").to_string()
 }
